65013cd2067a9ea0e929ec34def27c614d6d2346
thiago <thiago@pop-os.localdomain>
2020-10-27 15:57:49 -0300

    Creating methods show, update, delete .

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -6,9 +6,21 @@ module.exports = {
         const product = await Product.find();
         return res.json(product);
     },
+    async show(req, res){
+        const product = await Product.findById(req.params.id);
+        return res.json(product);
+    },
     async store(req, res){
         const product = await Product.create(req.body);
         return res.json(product);
     },
+    async update(req,res){
+        const product = await Product.findOneAndUpdate(req.params.id, req.body, { new:true });
+        return res.json(product);
+    },
+    async destroy(req,res){
+        await Product.findByIdAndRemove(req.params.id);
+        return res.send(`Sucess, deleted id=${req.params.id}!`);
+    }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,10 @@ const routes = express.Router();
 const ProductControllers = require('./controllers/ProductController')
 
 routes.get('/products', ProductControllers.index);
+routes.get('/products/:id', ProductControllers.show);
 routes.post('/products', ProductControllers.store);
+routes.put('/products/:id', ProductControllers.update);
+routes.delete('/products/:id', ProductControllers.destroy);
+
 
 module.exports = routes;
